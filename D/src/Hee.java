b07698290799aa9701dbb0ddc9f6b17e2e535026
JangMyounghee <zldkszltm2@hanmail.net>
2015-03-15 11:36:25 +0900

    되라

--- D/src/Hee.java
+++ D/src/Hee.java
@@ -2,7 +2,8 @@
 public class Hee {
 
 	public static void main(String[] args) {
-	System.out.println("hhh");	// TODO Auto-generated method stub
+	System.out.println("hhh");
+	System.out.println("hhhh");// TODO Auto-generated method stub
 
 	}
 
